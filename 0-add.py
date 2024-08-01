ee52834e5995881bc64d44c13899d91a10a2f122
gboigwe <geakande@gmail.com>
2024-08-02 00:05:30 +0100

    Re structuring a function to be annotatedwq

--- 0-add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""function add typed-annotation"""
-
-
-def add(a: float, b: float) -> float:
-    """Function that adds two numbers together"""
-    if type(a) and type(b) != float:
-        print("Not a float, use float number")
-    return float(a) + float(b)
