86a871a901ea604f9e932dc94269a886e4a0b776
gboigwe <geakande@gmail.com>
2024-08-01 17:08:38 +0100

    An annotation function that concat two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Type-annotated function to concat
+that takes a string str1 and a string str2 as arguments
+"""
+
+
+def concat(str1: str, str2: str):
+    """Takes in two argument for concatenation"""
+    return str1 + str2
