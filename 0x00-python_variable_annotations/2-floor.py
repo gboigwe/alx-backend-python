33e2edb5929b23715d0fbafba727fe2c7ca4365d
gboigwe <geakande@gmail.com>
2024-08-01 17:27:50 +0100

    A type-annotation function that builds reusable

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""function floor typed-annotation to approximate
+figures to nearest end
+"""
+
+
+def floor(n: float) -> int:
+    """Function that rounnds numbers to end"""
+    return int(n)
---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotation function for a string"""
+
+
+def to_str(n: float) -> str:
+    """Func takes in args float and returns string"""
+    return str(n)
