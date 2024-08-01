12b2b063341d44740db71ebb9c2838a840934d82
gboigwe <geakande@gmail.com>
2024-08-02 00:10:03 +0100

    Re structuring a function to be annotatedwq

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -4,8 +4,8 @@ from typing import Any, Mapping, TypeVar, Union
 T = TypeVar('T')
 
 
-def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]
-        = None) -> Union[Any, T]:
+def safely_get_value(dct: Mapping, key: Any, default: Union[
+    T, None] = None) -> Union[Any, T]:
     """Function handling the type-annotated"""
     if key in dct:
         return dct[key]
