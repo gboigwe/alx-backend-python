79371a586177f40431d77d7a0e83b6f455d71fcf
gboigwe <geakande@gmail.com>
2024-08-01 16:23:50 +0100

    Writing python annotation

--- 0-add.py
+++ 0-add.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-def add(float(a), float(b)) :
-    if type(a) and type)b) != float:
+"""function add typed-annotation"""
+
+
+def add(a: float, b: float) -> float:
+    """Function that adds two numbers together"""
+    if type(a) and type(b) != float:
         print("Not a float, use float number")
-    return a + b
+    return float(a) + float(b)
