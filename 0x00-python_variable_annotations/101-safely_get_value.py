69b107d718c2340b7d43f87029cb247fef893579
gboigwe <geakande@gmail.com>
2024-08-02 00:01:44 +0100

    Re structuring a function to be annotatedwq

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 """Type-annotated function in advanced way"""
-from typing import Mapping, Any, Union, Typevar
-T = Typevar('T')
+from typing import Any, Mapping, TypeVar, Union
+T = TypeVar('T')
 
 
 def safely_get_value(dct: Mapping, key: Any, default: Union[T, None]
         = None) -> Union[Any, T]:
-    """Function haddling the type-annotated"""
+    """Function handling the type-annotated"""
     if key in dct:
         return dct[key]
     else:
