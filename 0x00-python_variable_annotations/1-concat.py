bc48a5e100e9a7f388ad34d50156b888de964cbc
gboigwe <geakande@gmail.com>
2024-08-01 17:10:31 +0100

    An annotation function that concat two strings

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -4,6 +4,6 @@ that takes a string str1 and a string str2 as arguments
 """
 
 
-def concat(str1: str, str2: str):
+def concat(str1: str, str2: str) -> str:
     """Takes in two argument for concatenation"""
     return str1 + str2
