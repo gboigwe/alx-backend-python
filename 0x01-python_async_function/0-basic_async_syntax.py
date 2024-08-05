c0dc20739aab26dac5e1e62039f81b8bb319fc18
gboigwe <geakande@gmail.com>
2024-08-05 13:41:55 +0100

    Writing a basic async syntax

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Function that perform asynchronous await concurrent"""
+
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """Creating a delay function that await async"""
+    d_delay: float = random.uniform(0, max_delay)
+    await asyncio.sleep(d_delay)
+    return d_delay
